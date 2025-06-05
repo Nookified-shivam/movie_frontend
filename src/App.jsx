06933b3f866a59d4ef7468c1cfc21fdf20b11d9f
Nookified-shivam <jhashivam141@gmail.com>
2025-06-05 21:04:38 +0530

    Page routing and navigation with react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 import MovieCard from './components/MovieCard'
 import Homepage  from './pages/home'
+import {Routes,Route} from "react-router-dom"
+import Favourites from './pages/favourite'
+import Navbar from './components/navbar'
 function App() {
 return(
-  <> 
-<Homepage/>
-  </>
+  <div>
+    <div className="navbar">
+<Navbar/>
+    </div>
+<main className="main-content">
+  <Routes>
+    <Route path='/' element=<Homepage/>/>
+    <Route path='/favourites' element=<Favourites/>/>
+  </Routes>
+</main>
+</div>
 )
 }
 export default App
---
+++ src/pages/favourite.jsx
@@ -0,0 +1,9 @@
+function Favourites(){
+    return(
+        <div className="favourites">
+           <h1>No favourites adedd yet</h1>
+           <h2>Start Adding your favourite movies here...</h2>
+        </div>
+    )
+}
+export default Favourites;
