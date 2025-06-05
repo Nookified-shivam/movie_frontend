6ee2acc3643d3f5bf5320454c06cd7b6e91f0a12
Nookified-shivam <jhashivam141@gmail.com>
2025-06-05 19:35:39 +0530

    Movie search implementation using useState

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,18 +1,23 @@
 import MovieCard from "../components/MovieCard";
-
-function Homepage(){
-    const movies=[
-        {id:1,name:"singham",releasedate:"10-10-2010"},
-        {id:2,name:"chulbul",releasedate:"20-10-2011"},
-        {id:3,name:"dabang",releasedate:"10-10-2012"}
+import { useState } from "react";
+function Homepage() {
+    const HandleSubmit= (e)=>{
+        e.preventDefault();       
+        alert(currval);
+    }
+    const [currval, setval] = useState("");
+    const movies = [
+        { id: 1, name: "singham", releasedate: "10-10-2010" },
+        { id: 2, name: "chulbul", releasedate: "20-10-2011" },
+        { id: 3, name: "dabang", releasedate: "10-10-2012" }
     ];
-    return(
+    return (
         <div className="movie-grid">
-            <form action="" className="movie-search">
-                <input type="text" placeholder="Type movie name..."/>
+            <form action="" className="movie-search" onSubmit={HandleSubmit}>
+                <input type="text" placeholder="Type movie name..." value={currval} onChange={(e) => (setval(e.target.value))} />
                 <button type="submit" className="submit-btn">submit</button>
             </form>
-            {movies.map(movie=>(<MovieCard movie={movie} key={movie.id}/>))}
+            {movies.map(movie => movie.name.toLowerCase().startsWith(currval) && (<MovieCard movie={movie} key={movie.id} />))}
         </div>
     );
 }
